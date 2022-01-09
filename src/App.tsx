163721a058aa2fb2f8da22e91043b84f7a344469
howard <451006700@qq.com>
2022-01-10 00:08:06 +0800

    feat: init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import faker from 'faker'
 
 function App() {
+  const name = faker.name.firstName(1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     { name }
     </div>
   );
 }
---
+++ src/Item.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type TProps = {
+
+}
+
+const Item = React.memo<TProps>(() => {
+  return <></>
+});
+
+
+export default Item;
