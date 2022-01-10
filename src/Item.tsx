db774764db34bdffbaf30667e2a478ed0d6b3963
chenghao <chenghao@lizhi.fm>
2022-01-10 18:22:47 +0800

    feat: implememnt infinited scroll for fixed height

--- src/Item.tsx
+++ src/Item.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 
 type TProps = {
-
+  item: any,
+  style: any,
+  ref: any
 }
 
-const Item = React.memo<TProps>(() => {
-  return <></>
-});
+const Item = React.memo<TProps>(forwardRef(({
+  item,
+  style
+}, ref: any) => {
+  return (
+    <div ref={ref} style={style} className="item" >
+      <div className="info" >
+        <span>{item.name}</span>
+        <span>Thumb Up: {item.thumbUp}</span>
+      </div>
+      <div className="pic" >
+        <img src={item.pic} alt="" />
+      </div>
+      <div className="comment" >{item.comment}</div>
+    </div>
+  )
+}));
 
 
 export default Item;
--- src/mock.ts
+++ src/mock.ts
@@ -1,3 +1,25 @@
-export default function() {
-  
+import faker, { fake } from 'faker';
+
+const GENERATE_COUNT = 20;
+
+type Item_Type = {
+  name: string;
+  comment: string;
+  pic: string;
+  thumbUp: number;
+  scrollY?: number;
+};
+
+export function generateItems(): Item_Type[] {
+  const list = [];
+  for (let i = 0; i < GENERATE_COUNT; i++) {
+    const item = {
+      name: fake('{{name.lastName}} {{name.firstName}}, {{name.suffix}}'),
+      comment: faker.lorem.text(),
+      pic: faker.image.cats(300, 100),
+      thumbUp: faker.datatype.number(),
+    };
+    list.push(item);
+  }
+  return list;
 }
